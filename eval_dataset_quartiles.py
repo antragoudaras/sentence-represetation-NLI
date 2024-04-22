698f831f71cf3150dfe6537689005462192c98f0
antragoudaras <antonistragoudaras@gmail.com>
2024-04-22 20:30:47 +0200

    update eval_dataset

--- eval_dataset_quartiles.py
+++ eval_dataset_quartiles.py
@@ -27,51 +27,26 @@ class QuartileDataloaderBuilder(DataLoaderBuilder):
     
     def quartile_dataset(self, dataset):
         #Sort the dataset based on the length of the premise and hypothesis
-        dataset1 = dataset.map(self.combine_length_calc)
-        dataset2 = dataset.map(self.combine_length_calc)
+        dataset = dataset.map(self.combine_length_calc)
         
         #Quartile Calculation
-        length = dataset1["avg_len"] #Get the length of the dataset
+        length = dataset["avg_len"] #Get the length of the dataset
         quartiles = np.quantile(length, self.quartile) #Calculate the quartile
-
-        #Filter the dataset based on the quartile
-        dataset1 = dataset.map(self.combine_length_calc)
-        
-        
-        
-        quantiled_dataset = []
-        for idx, quartile in enumerate(quartiles):
-            if idx == 0:
-                lower_bound = 0
-            else:
-                lower_bound = quartiles[idx-1]
-            upper_bound = quartile
-            #Append the quartile
-            quantiled_dataset.append(dataset1.filter(lambda x: lower_bound <= x["avg_len"] <= upper_bound))
-        
-        #Append the last quartile
-        quantiled_dataset.append(dataset1.filter(lambda x: quartiles[-1] <= x["avg_len"]))
-        
-        #Remove the length key
-        for subset in quantiled_dataset:
-            subset = subset.remove_columns("avg_len")
-        
-        
-        
+ 
         # Filter the dataset based on the quartile
-        quantiled_dataset2 = [
-            dataset2.filter(lambda x: lower_bound <= x["avg_len"] <= upper_bound)
+        quantiled_dataset = [
+            dataset.filter(lambda x: lower_bound <= x["avg_len"] < upper_bound)
             for lower_bound, upper_bound in zip([0] + list(quartiles), quartiles)
         ]
         
         # Append the last quartile
-        quantiled_dataset2.append(dataset2.filter(lambda x: quartiles[-1] <= x["avg_len"]))
+        quantiled_dataset.append(dataset.filter(lambda x: quartiles[-1] <= x["avg_len"]))
         
         # Remove the length key
-        quantiled_dataset2 = [subset.remove_columns("avg_len") for subset in quantiled_dataset2]
+        quantiled_dataset = [subset.remove_columns("avg_len") for subset in quantiled_dataset]
 
 
-        return quantiled_dataset2
+        return quantiled_dataset
     
 def set_seed(seed):
     np.random.seed(seed)
@@ -126,26 +101,28 @@ def main(args):
     quariles_list = args.quartile_list
     dataloader = QuartileDataloaderBuilder(quartile=quariles_list, **dataset_dict)
     test_loader = dataloader.get_dataloader("test")
-    quantiled_dataset = dataloader.quartile_dataset(test_loader)
+    quantiled_dataset = dataloader.quartile_dataset(test_loader.dataset)
     logging.info(f"Shortest dataset size: {len(quantiled_dataset[0])}")
     logging.info(f"Middle dataset size: {len(quantiled_dataset[1])}")
     logging.info(f"Longest dataset size: {len(quantiled_dataset[2])}")
 
     logging.info("Evaluating the model on SNLI dataset")
 
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser(description="Evaluate the model either on SNLI and/or the SentEval tasks")
-        parser.add_argument("checkpoint", type=str, help="Path to the model checkpoint")
-        parser.add_argument("--encoder", type=str, default="bilstm-max", help="Encoder type", choices=["baseline", "unilstm", "bilstm", "bilstm-max"])
-        parser.add_argument("--seed", type=int, default=1111, help="Random seed")
-        parser.add_argument("--num_workers", type=int, default=4, help="Number of workers for the dataloader")
-        parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
-        parser.add_argument("--quartile_list", type=list, default=[0.33, 0.66], help="Quartile list")
-        args = parser.parse_args()
+    
 
-        logging.basicConfig(level=logging.INFO,
-                            format="%(asctime)s: %(message)s",
-                            datefmt="%m/%d %I:%M:%S %p")
-        
-        main(args)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate the model either on SNLI and/or the SentEval tasks")
+    parser.add_argument("checkpoint", type=str, help="Path to the model checkpoint")
+    parser.add_argument("--encoder", type=str, default="bilstm-max", help="Encoder type", choices=["baseline", "unilstm", "bilstm", "bilstm-max"])
+    parser.add_argument("--seed", type=int, default=1111, help="Random seed")
+    parser.add_argument("--num_workers", type=int, default=4, help="Number of workers for the dataloader")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
+    parser.add_argument("--quartile_list", type=list, default=[0.33, 0.66], help="Quartile list")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s: %(message)s",
+                        datefmt="%m/%d %I:%M:%S %p")
+    
+    main(args)
     
